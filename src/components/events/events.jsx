929363c6e79af46b2479746ff1641b7c694aad71
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2023-10-14 15:40:10 -0400

    Removed Next Chapter

--- src/components/events/events.jsx
+++ src/components/events/events.jsx
@@ -7,18 +7,10 @@ const Eventss = () => {
     return (
         <div style={{ width: "70%", marginLeft: "auto", marginRight: "auto", marginTop: "50px" }}>
             <Typography component="div" variant="h5" fontSize={{ xs: 28, sm: 28, md: 20 }} fontWeight={"bold"} fontFamily={"Heebo, sans-serif"} color={"#ff9b02"}>
-                UP-COMING EVENTS
+                UPCOMING EVENTS
             </Typography>
             <Grid container sx={{ marginTop: 1, marginBottom: 30 }} justifyContent={"center"} spacing={4}>
-                <Grid item xs={12} md={6}   >
-                    <a href="https://www.nextchaptercon.com/" target='_blank' >
-                        <div data-aos="zoom-out-right" className=' p-3 rounded-xl event-card'>
-                            <Typography variant='body1' sx={{ color: "#b79cff", fontSize: 20, fontWeight: "bold" }}>10/07 - 10/07</Typography>
-                            <Typography variant='small' sx={{ color: "#ff9b02", fontSize: 20 }}>Next Chapter Dalton, GA</Typography>
-                        </div>
-                    </a>
-
-                </Grid>
+               
                 <Grid item xs={12} md={6}>
                     <a href="https://chattacon.org/" target='_blank' >
                         <div  data-aos="zoom-out-left" className='p-3 rounded-xl event-card'>
