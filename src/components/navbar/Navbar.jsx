46e78d778f56dad135393431ca27a8244f2afb8c
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2023-10-14 12:46:35 -0400

    Novels now has 1 category

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -137,14 +137,14 @@ const Navbar = () => {
                               Orbital Space
                             </Link>
                           </li>
-                          <li className="">
+                          {/*<li className="">
                             <a
                               className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
                               href="#"
                             >
                               2nd Category
                             </a>
-                          </li>
+                        </li>*/}
                         </ul>
                       </div>
                     </div>
@@ -234,7 +234,7 @@ const Navbar = () => {
                               Orbital Space
                             </Link>
                           </li>
-                          <li className="">
+                          {/*<li className="">
                             <a
                               className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
                               href="#"
@@ -242,7 +242,7 @@ const Navbar = () => {
                               2nd Category
                             </a>
                           </li>
-                          {/* <li className="">
+                          <li className="">
                               <a
                                 className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
                                 href="#"
