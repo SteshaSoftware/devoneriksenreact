bf32f3d6b0ead1349ed7f73f0cd4112b240ce2fd
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2023-10-15 17:22:21 -0400

    Added a margin btwn novels and sign up form

--- src/components/allnovels/AllNovels.jsx
+++ src/components/allnovels/AllNovels.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const AllNovels = () => {
 {/* to add novel duplicate grid item code to corresponding </grid>*/}
     return (
-        <Grid container spacing={3} justifyContent={"center"} alignItems={"center"} >
+        <Grid container spacing={3} justifyContent={"center"} alignItems={"center"} sx={{ marginBottom: "30px" }}>
             <Grid item md={3} sm={6} xs={12}  width={270} >
             <Link to={"/theft-of-fire"} width={270}>
             <Card className=' zoom' sx={{marginX: "auto",marginTop:"60px", maxWidth: 270, background:"none" }} >
