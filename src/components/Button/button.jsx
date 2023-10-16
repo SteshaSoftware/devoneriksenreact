b9df7f2238168173157c8752726422096c68f6e6
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2023-10-16 17:29:17 -0400

    Bookstore logos

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-
 import "./Button.css"
+import amazonimg from "../../images/amz white.png"
+import BandNimg from "../../images/BandN white.png"
 
 const Button = () => {
   return (
     <>
       <a target='_blank' href={"https://www.amazon.com/gp/product/B0CJHQ4LZN"}>
         <button class="buttn" style={{ marginTop: 25, marginLeft: 15 }}>
-          Amazon
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img src={amazonimg} alt="AMAZON" style={{width: '80%'}}/>
+          </div>
         </button>
       </a>
 
       <a target='_blank' href={"https://www.barnesandnoble.com/"}>
-        <button class="buttn" style={{ marginTop: 25, marginLeft: 15 }}>
-          Barnes and Noble
+      <button class="buttn" style={{ marginTop: 25, marginLeft: 15 }}>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img src={BandNimg} alt="Barnes and Novel" style={{width: '95%'}}/>
+          </div>
         </button>
       </a>
     </>
