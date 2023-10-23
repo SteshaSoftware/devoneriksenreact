3a8c39ceadfb441ac6412f73937da616b6e717ce
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2023-10-23 11:15:18 -0400

    Media Edits

--- src/components/Media/MediaContents.jsx
+++ src/components/Media/MediaContents.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './MediaContents.css'
 
-const MediaContent = ({ title, subtitle, link, date }) => {
+const MediaContent = ({ title, subtitle, content, date }) => {
     return (
         <div className="media-container">
             <span className="heading">{title}</span>
             <div className="content-container">
                 <span className="subheading">{subtitle}</span>
                     <iframe
-                        src={link}
+                        src={content}
                         className='vidstyle'
                         loading="lazy"
                     ></iframe>
--- src/pages/media.jsx
+++ src/pages/media.jsx
@@ -6,7 +6,7 @@ const Media = () => {
   return (
     <>
 
-      <div data-aos="fade-down" style={{ textAlign: "center", width: "100%", marginTop: "0px" }}>
+      <div data-aos="fade-down" style={{ width: "100%", marginTop: "0px" }}>
       <Box
         sx={{
       
@@ -18,8 +18,8 @@ const Media = () => {
       >
         <MediaContent 
         title= 'Chattanooga Daily Refresh'
-        subtitle = 'Devon joins the Daily Refresh to talk about his book and inspiration.'
-        link= 'http://sinclairstoryline.com/resources/embeds/jw8-embed.html?client=googima&file=https://content.uplynk.com/0bb90d57f47241939f5c956517168bef.m3u8&autostart=false' 
+        subtitle = 'Devon joins the Daily Refresh to talk about Theft of Fire and his inspiration.'
+        content= 'http://sinclairstoryline.com/resources/embeds/jw8-embed.html?client=googima&file=https://content.uplynk.com/0bb90d57f47241939f5c956517168bef.m3u8&autostart=false' 
         date = '2023-10-20'
         />
           
