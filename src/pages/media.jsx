fe9cd1675dbcd36fa74b28e278985ddc5a8a1e0e
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2023-10-23 18:51:50 -0400

    Added article, and article formating

--- src/pages/media.jsx
+++ src/pages/media.jsx
@@ -16,13 +16,32 @@ const Media = () => {
           marginRight: "auto",
         }}
       >
-        <MediaContent 
-        title= 'Chattanooga Daily Refresh'
-        subtitle = 'Devon joins the Daily Refresh to talk about Theft of Fire and his inspiration.'
-        content= 'http://sinclairstoryline.com/resources/embeds/jw8-embed.html?client=googima&file=https://content.uplynk.com/0bb90d57f47241939f5c956517168bef.m3u8&autostart=false' 
-        date = '2023-10-20'
-        />
-          
+           <div>
+            <MediaContent
+                title= 'Fire for the People'
+                subtitle = 'Interview with Devon with Johnny Schmidt, <i>Failure Mode</i>'
+                content = 'Early in October, I sat down with Devon Eriksen, one of a growing number of authors
+                 who are side-stepping the No Man’s Land of traditional publishing to take their work 
+                 directly to readers. He is the author of <i>Theft of Fire</i>, the first book of his planned Orbital 
+                 Space Trilogy, an old-fashioned space adventure with a modern feel—and a refreshingly apolitical 
+                 take on everything from AI to the male sex drive. We talk politics, publishing, institutional 
+                 decay, Tolkien and religion, Ayn Rand and propaganda, a future without government, the plight 
+                 of the working man (and writer), and the weird similarities between composing a novel and 
+                 writing code.'
+                link= 'https://failuremode.johnnyschmidt.com/p/fire-for-the-people-interview-with' 
+                date = '2023-10-23'
+                mediatype= "txt"
+            />
+            <MediaContent
+                title="Chattanooga Daily Refresh"
+                subtitle="Devon joins the Daily Refresh to talk about Theft of Fire and his inspiration."
+                link="http://sinclairstoryline.com/resources/embeds/jw8-embed.html?client=googima&file=https://content.uplynk.com/0bb90d57f47241939f5c956517168bef.m3u8&autostart=false"
+                date="2023-10-20"
+                mediatype= "vid"
+            />
+            {/* Add more MediaContent components as needed */}
+        </div>
+    
       </Box>
 
       </div>
